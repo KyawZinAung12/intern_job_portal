11ed42498ac8e26c095d48d54ae7cf0a32a78c98
heinminhtet8138 <heinminhtet8138@gmail.com>
2020-08-13 15:34:03 +0630

    db update

--- intern_job_portal.sql
+++ intern_job_portal.sql
@@ -1,14 +1,13 @@
 -- phpMyAdmin SQL Dump
--- version 4.9.2
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Aug 13, 2020 at 10:33 AM
--- Server version: 10.4.10-MariaDB
--- PHP Version: 7.3.12
+-- Generation Time: Aug 13, 2020 at 11:03 AM
+-- Server version: 10.4.11-MariaDB
+-- PHP Version: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -33,10 +32,31 @@ CREATE TABLE `job_posts` (
   `position_id` int(11) NOT NULL,
   `salary_id` int(11) NOT NULL,
   `location_id` int(11) NOT NULL,
-  `description` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` date NOT NULL DEFAULT current_timestamp()
+  `description` text COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `job_posts`
+--
+
+INSERT INTO `job_posts` (`id`, `position_id`, `salary_id`, `location_id`, `description`, `created_at`, `updated_at`) VALUES
+(1, 1, 2, 2, 'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2020-08-13 09:01:17', '2020-08-13 09:01:17'),
+(2, 2, 1, 3, 'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2020-08-13 09:01:17', '2020-08-13 09:01:17'),
+(3, 2, 1, 3, 'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2020-08-13 09:01:17', '2020-08-13 09:01:17'),
+(4, 1, 3, 2, 'What is Lorem Ipsum?\r\nLorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.', '2020-08-13 09:01:17', '2020-08-13 09:01:17');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `job_request`
+--
+
+CREATE TABLE `job_request` (
+  `id` int(11) NOT NULL,
+  `job_post_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
 -- --------------------------------------------------------
@@ -48,9 +68,17 @@ CREATE TABLE `job_posts` (
 CREATE TABLE `job_types` (
   `id` int(11) NOT NULL,
   `type` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` date NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `job_types`
+--
+
+INSERT INTO `job_types` (`id`, `type`, `created_at`, `updated_at`) VALUES
+(1, 'Full Time', '2020-08-13 08:39:32', '2020-08-13 08:39:32'),
+(2, 'Part Time', '2020-08-13 08:39:32', '2020-08-13 08:39:32');
 
 -- --------------------------------------------------------
 
@@ -61,9 +89,18 @@ CREATE TABLE `job_types` (
 CREATE TABLE `locations` (
   `id` int(11) NOT NULL,
   `name` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` date NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `locations`
+--
+
+INSERT INTO `locations` (`id`, `name`, `created_at`, `updated_at`) VALUES
+(1, 'Yangon', '2020-08-13 08:40:47', '2020-08-13 08:40:47'),
+(2, 'Mandalay', '2020-08-13 08:40:47', '2020-08-13 08:40:47'),
+(3, 'Nay Pyi Taw', '2020-08-13 08:41:05', '2020-08-13 08:41:05');
 
 -- --------------------------------------------------------
 
@@ -79,6 +116,18 @@ CREATE TABLE `positions` (
   `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
 
+--
+-- Dumping data for table `positions`
+--
+
+INSERT INTO `positions` (`id`, `name`, `photo`, `created_at`, `updated_at`) VALUES
+(1, 'Web Developer', 'position_img/1.jpg', '2020-08-13 08:43:18', '2020-08-13 08:43:18'),
+(2, 'Android Developer', 'position_img/2.jpg', '2020-08-13 08:43:18', '2020-08-13 08:43:18'),
+(3, 'Flutter Developer', 'position_img/3.jpg', '2020-08-13 08:45:07', '2020-08-13 08:45:07'),
+(4, 'C# Developer', 'position_img/4.jpg', '2020-08-13 08:45:07', '2020-08-13 08:45:07'),
+(5, 'Full Stack Developer', 'position_img/5.jpg', '2020-08-13 08:45:07', '2020-08-13 08:45:07'),
+(6, 'Network Engineer', 'position_img/6.jpg', '2020-08-13 08:45:07', '2020-08-13 08:45:07');
+
 -- --------------------------------------------------------
 
 --
@@ -87,10 +136,20 @@ CREATE TABLE `positions` (
 
 CREATE TABLE `salaries` (
   `id` int(11) NOT NULL,
-  `amount` int(11) NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` int(11) NOT NULL DEFAULT current_timestamp()
+  `amount` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `salaries`
+--
+
+INSERT INTO `salaries` (`id`, `amount`, `created_at`, `updated_at`) VALUES
+(1, '0-300000', '2020-08-13 08:47:22', '2020-08-13 08:47:22'),
+(2, '300000-500000', '2020-08-13 08:47:22', '2020-08-13 08:47:22'),
+(3, '500000-800000', '2020-08-13 08:47:22', '2020-08-13 08:47:22'),
+(4, '800000-1000000', '2020-08-13 08:47:22', '2020-08-13 08:47:22');
 
 -- --------------------------------------------------------
 
@@ -108,9 +167,17 @@ CREATE TABLE `users` (
   `address` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `photo` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
   `cv` text COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` date NOT NULL DEFAULT current_timestamp(),
-  `updated_at` int(11) NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `name`, `email`, `phone`, `gender`, `dob`, `address`, `photo`, `cv`, `created_at`, `updated_at`) VALUES
+(1, 'Mg Mg ', 'mgmg@gmail.com', 983484343, 'Male', '2020-08-18', 'Yangon, Hlaing', 'user_img/1.jpg', 'cv/1.pdf', '2020-08-13 08:56:59', '2020-08-13 08:56:59'),
+(2, 'Hla Hla', 'hlahla@gmail.com', 98765432, 'Female', '2020-08-25', 'Mandalay, Mahar Aung Myay', 'user_img/2.jpg', 'cv/2.pdf', '2020-08-13 08:56:59', '2020-08-13 08:56:59');
 
 --
 -- Indexes for dumped tables
@@ -120,6 +187,12 @@ CREATE TABLE `users` (
 -- Indexes for table `job_posts`
 --
 ALTER TABLE `job_posts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `job_request`
+--
+ALTER TABLE `job_request`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -160,37 +233,43 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `job_posts`
 --
 ALTER TABLE `job_posts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT for table `job_request`
+--
+ALTER TABLE `job_request`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
 
 --
 -- AUTO_INCREMENT for table `job_types`
 --
 ALTER TABLE `job_types`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- AUTO_INCREMENT for table `locations`
 --
 ALTER TABLE `locations`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `positions`
 --
 ALTER TABLE `positions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `salaries`
 --
 ALTER TABLE `salaries`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
